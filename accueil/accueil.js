dbacc7cbf95766844e77b0471848b925584f47e8
Tom Vieillard-Grelet <tom.vieillard.grelet@gmail.com>
2021-01-11 07:11:09 +0000

    woaaaaa

--- accueil/accueil.js
+++ accueil/accueil.js
@@ -4,11 +4,3 @@ function macopie1() {
   document.execCommand("Copy");
   alert("L'adresse mail a été copié");   
 }
-
-
-// function macopie1() {
-//   var copyText = document.getElementById("macopie1");
-//   copyText.select();
-//   document.execCommand("Copy");
-//   alert("L'adresse mail a été copié");   
-// }
