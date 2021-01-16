d70c6aa62dabdb31e1c41bbfff1763c5e2c71c4e
Tom Vieillard-Grelet <tom.vieillard.grelet@gmail.com>
2021-01-16 12:29:24 +0000

    y a eu des truc new

--- accueil/accueil.js
+++ accueil/accueil.js
@@ -4,3 +4,20 @@ function macopie1() {
   document.execCommand("Copy");
   alert("L'adresse mail a été copié");   
 }
+
+
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
